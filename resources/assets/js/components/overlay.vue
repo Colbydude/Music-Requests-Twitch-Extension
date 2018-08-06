ebad95f1b93540771f72fd28209bd75a1e1b34d2
Colby Terry <colbydude@gmail.com>
2018-08-05 22:07:58 -0700

    Viewer styling.

--- resources/assets/js/components/overlay.vue
+++ resources/assets/js/components/overlay.vue
@@ -1,17 +1,34 @@
 <template>
-    <div id="extension-overlay">
-        <div class="modal-shim" v-if="modalActive">
-            <div class="modal">
-                <div class="modal-title">Request A Song</div>
-                <div class="modal-footer">
-                    <button class="btn btn-transparent" @click="toggleModal">Cancel</button>
+    <div id="extension-overlay" @mouseover="buttonActive = true" @mouseleave="buttonActive = false">
+        <div class="menu-container">
+            <div class="menu" v-if="menuActive">
+                <div id="song-request" class="panel">
+                    <h2 class="tile">
+                        <div class="tile-content">Request A Song</div>
+                    </h2>
+                    <div class="search-bar">
+                        <form @submit="search">
+                            <input type="text" v-model="input" class="form-control" placeholder="Search user's library">
+                            <button class="btn btn-blue-dark btn-small">
+                                <i class="fa fa-search"></i>
+                            </button>
+                            <i class="clear fas fa-times-circle" @click="clear" v-if="input.length > 0"></i>
+                        </form>
+                    </div>
+                    <div class="queue">
+                        <h2 class="tile">
+                            <div class="tile-content">Current Queue:</div>
+                        </h2>
+                        <section>
+                            <ul>
+                            </ul>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="btn-container">
-            <button class="btn btn-blue-dark btn-menu" @click="toggleModal">
-                <i class="fas fa-fw fa-times" v-if="modalActive"></i>
+            <button class="btn btn-blue-dark btn-menu" @click="toggleMenu">
+                <i class="fas fa-fw fa-times" v-if="menuActive"></i>
                 <i class="fas fa-fw fa-music" v-else></i>
             </button>
         </div>
@@ -22,18 +39,38 @@
     export default {
         data () {
             return {
-                modalActive: false
+                buttonActive: false,
+                input: '',
+                menuActive: false
             };
         },
 
         methods: {
             /**
-             * Toggles the modal being active.
+             * Clear the input.
              *
              * @return {void}
              */
-            toggleModal () {
-                this.modalActive = !this.modalActive;
+            clear () {
+                this.input = '';
+            },
+
+            /**
+             * Search for a song in the library.
+             *
+             * @return {void}
+             */
+            search (event) {
+                event.preventDefault();
+            },
+
+            /**
+             * Toggles the menu.
+             *
+             * @return {void}
+             */
+            toggleMenu () {
+                this.menuActive = !this.menuActive;
             }
         }
     }
